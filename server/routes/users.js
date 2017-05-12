465645c80f397756dece04cccabceff5c2571ef6
Bahaa Zidan <Bahaa Zidan>
2017-05-12 16:19:35 +0200

    try to catch the passport error

--- server/routes/users.js
+++ server/routes/users.js
@@ -27,9 +27,9 @@ router.route('/')
 
 router.post('/register', (req, res) => {
   users.register(new users({ username : req.body.username }), req.body.password, (err, val) => {
-    if (err) return next(err);
+    if (err) return res.status(500).json({status: 'Authentication Problem'});
     val.save((err,user) => {
-      if (err) return next(err);
+      if (err) return res.status(500).json({status: 'Authentication Problem'});
       passport.authenticate('local')(req, res, () => { 
         return res.status(200).json({status: 'Registration Successful!'});
       });
